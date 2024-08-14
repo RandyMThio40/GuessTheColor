7b3d5c303e0516ea4800875a9e5633cd97aa70c0
RandyMThio40 <37589527+RandyMThio40@users.noreply.github.com>
2024-08-14 18:40:30 -0400

    added new expanding animation

--- src/components/colors-game/colorsGame.js
+++ src/components/colors-game/colorsGame.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef, useLayoutEffect} from 'react';
+import React,{useState,useRef, useLayoutEffect, useEffect} from 'react';
 import './colorsGame.css';
 
 const genRand = (max) => {
@@ -17,16 +17,40 @@ const getRandomHex = () => {
     return hex
 }
 
+
+
 export const ColorsGame = () => {
     const [options,setOptions] = useState([]);
     const coloredWindow = useRef();
     const [score,setScore] = useState(0);
     const [round,setRound] = useState(0);
+    const animatedBG = useRef();
+    const TDuration = 800
+    const prevColor = useRef(undefined)
 
-    const handleSubmit = (val) => {
-        if(document.documentElement.style.getPropertyValue('--game-color') === val) setScore(prev=>prev+1)
-        setRound(prev=>prev+1)
+    const handleSubmit = (val,e) => {
+        if(document.documentElement.style.getPropertyValue('--game-color') === val) e.target.style.color = "green"
+        animatedBG.current.style.backgroundColor = val
+        animatedBG.current.style.setProperty('--clip-value',`200%`)
+        animatedBG.current.style.transitionDuration = `${TDuration}ms`;
+        setTimeout(()=>{
+            if(document.documentElement.style.getPropertyValue('--game-color') === val) setScore(prev=>prev+1)
+            prevColor.current = val;
+            setRound(prev=>prev+1)
+        },TDuration)
     }
+
+    const getCoordinates = (e) => {
+        animatedBG.current.style.setProperty('--x',`${e.pageX}px`);
+        animatedBG.current.style.setProperty('--y',`${e.pageY}px`);
+    }
+
+    const resetBG = () => {
+        animatedBG.current.style.transitionDuration = `0ms`;
+        animatedBG.current.style.setProperty('--clip-value',`0%`)
+    }
+
+
     
     useLayoutEffect(()=>{
         const genOptions = () => {
@@ -38,10 +62,17 @@ export const ColorsGame = () => {
         }
         let arr = genOptions()
         setOptions(arr);
+        resetBG()
         document.documentElement.style.setProperty('--game-color',arr[genRand(arr.length-1)])
+        return(()=>{
+
+            document.body.style.backgroundColor = prevColor.current
+        })
     },[round])
+
     return(
-        <>
+        <>  
+            <div ref={animatedBG} className='animatedBg'></div>
             <div scoreboard="" >
                 {score} of {round} rounds correct
             </div>
@@ -50,7 +81,7 @@ export const ColorsGame = () => {
             <div className="color-options">
                 {options.map((item,idx)=>{
                     return(
-                        <button onClick={()=>handleSubmit(item)} key={idx} >{item}</button>
+                        <button onTouchStart={(e)=>{console.log("touch start",e.touches)}} onTouchMove={(e)=>{console.log("touch move",e.touches)}} onMouseMove={getCoordinates} onClick={(e)=>handleSubmit(item,e)} key={idx} >{item}</button>
                     )
                 })}
             </div>
